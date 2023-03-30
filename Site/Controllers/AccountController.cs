9e13722459e4a8e94c35677267438156ea66f308
Bruno Seraguza <shagrathkampf@gmail.com>
2023-03-29 23:33:13 -0300

    #TelaLogin1.0

---
+++ Site/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Site.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
