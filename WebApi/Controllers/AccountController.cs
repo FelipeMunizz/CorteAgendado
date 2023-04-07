f6ad35012cbed5ab8b2a5ccb44706c67ac9b393e
Felipe Muniz <lipe.baterra@gmail.com>
2023-04-07 00:49:02 -0300

    Inicio da implementação dos Controllers

---
+++ WebApi/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    [HttpGet("TesteConexao")]
+    public ActionResult<string> TesteConexao()
+    {
+        return $"Api acessada: {DateTime.Now.ToString("dd-MM-yyyy HH:mm")}";
+    }
+}
---
+++ WebApi/Controllers/BarbeariaController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Interfaces;
+using Entities.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarbeariaController : ControllerBase
+    {
+        private readonly IBarbearia _barbearia;
+
+        public BarbeariaController(IBarbearia barbearia)
+        {
+            _barbearia = barbearia;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Barbearia>>> Get()
+        {
+            return await _barbearia.List();
+        }
+    }
+}
